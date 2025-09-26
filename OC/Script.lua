4c77779dbd3b4bdc72f83cbf38edcb0e7d6bc91f
Zoprever <zoprever@gmail.com>
2025-09-25 21:32:29 -0700

    Update Script.lua

--- OC/Script.lua
+++ OC/Script.lua
@@ -4,3 +4,15 @@
 -- Remove
 
 -- Set
+function Set(Path, CFrame, Size)
+local args = {
+	{
+		{
+			Path,
+			CFrame or Path.CFrame,
+			Size or Path.Size
+		}
+	}
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("MoveObject"):InvokeServer(unpack(args))
+end
