d95faa611024465f32176d0e1011edaa67805f88
Zoprever <zoprever@gmail.com>
2025-09-26 20:36:15 -0700

    Update Script.lua

--- OC/Script.lua
+++ OC/Script.lua
@@ -9,6 +9,7 @@ game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("AddObj
 end
 -- Remove
 function Remove(Path)
+	print("Removing " .. Path.Name .. " On " .. Path)
 	local args = {
 	{
 		Path
@@ -16,16 +17,38 @@ function Remove(Path)
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("DeleteObject"):InvokeServer(unpack(args))
 end
--- Set
-function Set(Path, CFrame, Size)
+-- Move
+function Move(Path, Cframe, Size)
 local args = {
 	{
 		{
 			Path,
-			CFrame or Path.CFrame,
+			Cframe or Path.CFrame,
 			Size or Path.Size
 		}
 	}
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("MoveObject"):InvokeServer(unpack(args))
+end
+-- Get Object --
+-- Variables
+local Storage = game.Workspace.Obbies[Player.Name].
+
+-- Find Object With No Attributes
+function FOWNA()
+    for _, obj in ipairs(Storage:GetChildren()) do
+        if next(obj:GetAttributes()) == nil then
+            return obj
+        end
+    end
+    return nil
+end
+-- Find Object With A Attribute
+function FOWAA(Attribute, Value)
+    for _, obj in ipairs(Storage:GetChildren()) do
+        if obj:GetAttribute(Attribute) == Value then
+            return obj
+        end
+    end
+    return nil
 end
