15beaa730a91a62b47321bd038536cd570a1af21
Zoprever <zoprever@gmail.com>
2025-09-26 23:08:54 -0700

    Update Script.lua

--- OC/Script.lua
+++ OC/Script.lua
@@ -32,7 +32,7 @@ game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("MoveOb
 end
 -- Get Object --
 -- Variables
-local Storage = game.Workspace.Obbies[Player.Name].Items
+local Storage = game.Workspace.Obbies.SAREU_DX.Items
 -- Find Object With No Attributes
 function FOWNA()
     for _, obj in ipairs(Storage:GetChildren()) do
