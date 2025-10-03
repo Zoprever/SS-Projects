2ff50276b3ff76a83f3f78417bb5d12c2b742611
Zoprever <zoprever@gmail.com>
2025-10-03 02:40:51 -0700

    Update Scriptable block.lua

--- OC/Scriptable block.lua
+++ OC/Scriptable block.lua
@@ -1,5 +1,6 @@
 -- Main UI
-
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/Arceus-X-UI-Library/main/source.lua"))()
+lib:SetTitle("Scriptable Block")
 -- Variables
 
 -- Sections --
